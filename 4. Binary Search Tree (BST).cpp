d2892955d82cb98d1d583180ecf6c3d01a74eb94
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-29 01:40:48 +0600

    Update 4. Binary Search Tree (BST).cpp

--- 4. Binary Search Tree (BST).cpp
+++ 4. Binary Search Tree (BST).cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class BSTNode {
+public:
+    int data;
+    BSTNode* left;
+    BSTNode* right;
+    BSTNode(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+class BST {
+private:
+    BSTNode* root;
+
+public:
+    BST() : root(nullptr) {}
+
+    void insert(int val) {
+        root = insertNode(root, val);
+    }
+
+    BSTNode* insertNode(BSTNode* node, int val) {
+        if (!node) return new BSTNode(val);
+        if (val < node->data) node->left = insertNode(node->left, val);
+        else node->right = insertNode(node->right, val);
+        return node;
+    }
+
+    void inorderTraversal() {
+        inorder(root);
+    }
+
+    void inorder(BSTNode* node) {
+        if (!node) return;
+        inorder(node->left);
+        cout << node->data << " ";
+        inorder(node->right);
+    }
+};
 
