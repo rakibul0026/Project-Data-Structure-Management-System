0378e1dc188c119ed9deb67847a75ef48adcb09d
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-29 01:38:31 +0600

    Update 2. Stack.cpp

--- 2. Stack.cpp
+++ 2. Stack.cpp
@@ -1,1 +1,42 @@
+#define MAX 100
+#include <iostream>
+using namespace std;
+
+class Stack {
+private:
+    int arr[MAX];
+    int top;
+
+public:
+    Stack() { top = -1; }
+
+    void push(int val) {
+        if (top >= (MAX - 1)) {
+            cout << "Stack Overflow\n";
+        } else {
+            arr[++top] = val;
+        }
+    }
+
+    int pop() {
+        if (top < 0) {
+            cout << "Stack Underflow\n";
+            return -1;
+        } else {
+            int val = arr[top--];
+            return val;
+        }
+    }
+
+    void display() {
+        if (top < 0) {
+            cout << "Stack is Empty\n";
+        } else {
+            for (int i = top; i >= 0; i--) {
+                cout << arr[i] << " ";
+            }
+            cout << endl;
+        }
+    }
+};
 
